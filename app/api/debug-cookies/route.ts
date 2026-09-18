2f782b2496c06c0ffbadfcff69c252e08958ba83
NNAATTtx9 <tnat2576@gmail.com>
2026-09-18 17:18:54 +0800

    Create route.ts

---
+++ app/api/debug-cookies/route.ts
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const allCookies = cookies().getAll();
+  return NextResponse.json(allCookies);
+}
+
