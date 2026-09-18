05acd581db11c4dddd3a64d38bcd11da4c95e82e
NNAATTtx9 <tnat2576@gmail.com>
2026-09-18 21:07:35 +0800

    Update route.ts

--- app/api/debug-cookies/route.ts
+++ app/api/debug-cookies/route.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const allCookies = cookies().getAll();
+  const allCookies = (await cookies()).getAll();
   return NextResponse.json(allCookies);
 }
 
