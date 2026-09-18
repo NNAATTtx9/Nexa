b359b92d074781f231f8129e7e486017c6dabce7
NNAATTtx9 <tnat2576@gmail.com>
2026-09-18 17:15:34 +0800

    Delete app/debug-cookies directory

--- app/debug-cookies/Route.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const allCookies = cookies().getAll();
-  return NextResponse.json(allCookies);
-}
-
